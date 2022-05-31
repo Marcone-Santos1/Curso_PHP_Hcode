e0060828e49aa20b21eb3d5fb21cca065f4f8d96
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2022-05-31 12:26:50 -0300

    Arrays e dowhile

---
+++ arrays/exemplo-01.php
@@ -0,0 +1,24 @@
+<?php 
+
+// vetor é um 'Array' de uma dimensão, ou seja, não é um array
+
+$frutas =  array("Laranja", "Manga", "Banana", "Limão", "Melancia");
+
+// printar array:
+print_r($frutas);
+
+$carros[0][0] = "GM";
+$carros[0][1] = "Cobalt";
+$carros[0][2] = "Onix";
+$carros[0][3] = "Camaro";
+
+$carros[1][0] = "Ford";
+$carros[1][1] = "Fiesta";
+$carros[1][2] = "Fusion";
+$carros[1][3] = "Eco Sport";
+
+echo $carros[0][3];
+
+echo end($carros[1]);
+
+?>
