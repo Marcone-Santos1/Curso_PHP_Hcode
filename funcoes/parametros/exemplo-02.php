39b07f2d1ae0cd7701364b8db4ad28aecc1be471
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2022-06-03 09:01:29 -0300

    exemplo-02.php

--- funcoes/parametros/exemplo-02.php
+++ funcoes/parametros/exemplo-02.php
@@ -1,1 +1,18 @@
 <?php
+
+// passagem de valor por referência
+
+$a = 10;
+
+function trocaValor(&$a){
+// & = passagem de parametro por referência
+
+    $a += 50;
+
+    return $a;
+
+}
+
+echo $a;
+echo "<br>";
+echo trocaValor($a);
