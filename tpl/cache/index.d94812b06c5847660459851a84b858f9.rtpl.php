5c58dd92af5971d716fe3f6255d514efb42812b5
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2022-07-18 15:05:20 -0300

    tpl add

---
+++ tpl/cache/index.d94812b06c5847660459851a84b858f9.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Hello <?php echo htmlspecialchars( $name, ENT_COMPAT, 'UTF-8', FALSE ); ?></h1>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo illum quos vel quod non libero sunt harum nesciunt. Id inventore ratione facere voluptatem reiciendis consectetur corporis. Cum, eos dolor. Reiciendis!
+</p>
+
+<p>
+    PHP: <?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+</p>
